0a94662fa5f6ca157c1d5f517677ba0cd6d45b40
Paulo Torrealba <paulo@buscalibre.com>
2023-02-04 06:05:21 -0300

    Revisa si hay links repetidos para descartar que hay productos repetidos

--- src/pages/Cerebro/components/RevisaRepetidos.js
+++ src/pages/Cerebro/components/RevisaRepetidos.js
@@ -7,19 +7,24 @@ function RevisaRepetidos() {
 	const [cantidad, setCantidad] = useState();
 
 	useEffect(() => {
-		// fetch( DB ).then( (x) => x.json() ).then( (x) => {
-		// console.debug( x );
-		const actual = ["bacon", "ipsum", "dolor", "amet", "strip", "steak", "chislic", "andouille", "burgdoggen", "tbone", "strip", "steak", "picanha", "boudin", "short", "loin", "hamburger"];
-		// const actual = ["bacon", "ipsum", "dolor", "amet", "strip", "chislic", "andouille", "burgdoggen", "tbone", "steak", "picanha", "boudin", "short", "loin", "hamburger"];
+		fetch( DB ).then( (x) => x.json() ).then( (x) => {
+			const lasurls = [];
+			for ( let count = 1; count <= x.values.length - 1; count++ ) {
+				const p = {
+					link: x.values[count][7],
+				};
+				lasurls.push( p.link );
+			}
 
-		const repetidos = function ( array ) {
-			return array.filter((currentValue, currentIndex) => array.indexOf(currentValue) !== currentIndex);
-		};
-		const algo = repetidos(actual);
-		const lacan = algo.length;
-		setLosrepetidos( algo );
-		setCantidad( lacan );
-		// } );
+			const repetidos = function ( array ) {
+				return array.filter((currentValue, currentIndex) => array.indexOf(currentValue) !== currentIndex);
+			};
+			console.debug( lasurls );
+			const algo = repetidos(lasurls);
+			const lacan = algo.length;
+			setLosrepetidos( algo );
+			setCantidad( lacan );
+		} );
 	}, []);
 
 	return (
