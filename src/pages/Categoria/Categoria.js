9f3de6fb678584b36f01cd5b8bc2de47d7ec5989
Paulo Torrealba <paulo@buscalibre.com>
2023-02-07 18:12:29 -0300

    trae la data del json local

--- src/pages/Categoria/Categoria.js
+++ src/pages/Categoria/Categoria.js
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { useLocation } from "react-router-dom";
-import { DB, NOMBRETIENDA } from "../../assets/js/CONST";
+import { NOMBRETIENDA } from "../../assets/js/CONST";
 
 import Loading from "../../components/Loading/Loading";
 import Producto from "../../components/Producto/Producto";
 
 import {
 	armonizarURL,
-	// devuelveAlHome,
 	precio,
 	// isMobile,
 	humanizaString,
 } from "../../Helpers/Helpers";
+import eljson from "../../assets/json/inventario.json";
 
 function Categoria() {
 	const categoria = useLocation().pathname.split("/")[2];
@@ -21,46 +21,42 @@ function Categoria() {
 	const categoriaHumanizada = humanizaString(categoria);
 
 	useEffect(() => {
-		fetch( DB ).then((db) => db.json()).then((db) => {
-			setLoading(true);
-			const data = [];
-			for ( let count = db.values.length - 1; count >= 1; count-- ) {
-				const resumen = db.values[count];
+		setLoading(true);
+		const data = [];
+		for ( let count = eljson.values.length - 1; count >= 1; count-- ) {
+			const resumen = eljson.values[count];
 
-				const p = {
-					id: resumen[0],
-					nombre: resumen[1].replaceAll("SHEIN ", ""),
-					precio: () => {
-						return precio( resumen[2] );
-					},
-					fechaentrega: resumen[3],
-					imagen: resumen[4].split(",//")[0],
-					categoria: resumen[9],
-					color: () => {
-						let color = resumen[11];
-						if ( !color ) {
-							color = "no especifica";
-						}
-						return color;
-					},
-				};
+			const p = {
+				id: resumen[0],
+				nombre: resumen[1].replaceAll("SHEIN ", ""),
+				precio: precio( resumen[2] ),
+				fechaentrega: resumen[3],
+				imagen: resumen[4].split(",//")[0],
+				categoria: resumen[9],
+				color: () => {
+					let color = resumen[11];
+					if ( !color ) {
+						color = "no especifica";
+					}
+					return color;
+				},
+			};
 
-				let tempCategoria = categoria;
-				tempCategoria = tempCategoria.replaceAll("-", " ");
-				let tempPCategoria = p.categoria;
-				tempPCategoria = tempPCategoria.toLowerCase();
+			let tempCategoria = categoria;
+			tempCategoria = tempCategoria.replaceAll("-", " ").replaceAll("%20", " ");
+			tempCategoria = tempCategoria.toLowerCase();
+			let tempPCategoria = p.categoria;
+			tempPCategoria = tempPCategoria.toLowerCase();
 
-				if ( tempCategoria === tempPCategoria ) {
-					data.push( p );
-				}
+			if ( tempCategoria === tempPCategoria ) {
+				data.push( p );
 			}
-
-			setElproducto(data);
-			// setLoading(true);
-		});
+		}
+		setElproducto(data);
+		setLoading(false);
 
 		armonizarURL(2);
-	});
+	}, [categoria]);
 
 	if ( elproducto ) {
 		return (
@@ -77,8 +73,7 @@ function Categoria() {
 									key={prod.id}
 									id={prod.id}
 									nombre={prod.nombre}
-									precio={prod.precio()}
-									// fechaentrega={prod.fechaentrega}
+									precio={prod.precio}
 									imagen={prod.imagen}
 								/>
 							);
