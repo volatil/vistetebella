c5064864a93c0b9e7f019aa789a5de95a9b222cf
Paulo Torrealba <paulo@buscalibre.com>
2023-02-07 17:54:41 -0300

    usando el json local

--- src/pages/Tienda/Tienda.js
+++ src/pages/Tienda/Tienda.js
@@ -1,53 +1,43 @@
 import { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
-import { DB, NOMBRETIENDA } from "../../assets/js/CONST";
+import { NOMBRETIENDA } from "../../assets/js/CONST";
 
 import Loading from "../../components/Loading/Loading";
 import Producto from "../../components/Producto/Producto";
 
 import { precio } from "../../Helpers/Helpers";
+import eljson from "../../assets/json/inventario.json";
 
 function Tienda() {
 	const [elproducto, setElproducto] = useState();
 	const [loading, setLoading] = useState();
 
 	useEffect(() => {
-		fetch( DB ).then((db) => db.json()).then((db) => {
-			setLoading(true);
-			const data = [];
+		const data = [];
+		for ( let count = eljson.values.length - 1; count >= 1; count-- ) {
+			const resumen = eljson.values[count];
 
-			for ( let count = db.values.length - 1; count >= 1; count-- ) {
-				const resumen = db.values[count];
+			const p = {
+				id: resumen[0],
+				nombre: resumen[1].replaceAll("SHEIN ", ""),
+				precio: () => {
+					return precio( resumen[2] );
+				},
+				fechaentrega: resumen[3],
+				imagen: resumen[4].split(",//")[0],
+				color: () => {
+					let color = resumen[11];
+					if ( !color ) {
+						color = "no especifica";
+					}
+					return color;
+				},
+			};
 
-				const p = {
-					id: resumen[0],
-					nombre: resumen[1].replaceAll("SHEIN ", ""),
-					precio: () => {
-						return precio( resumen[2] );
-					},
-					fechaentrega: resumen[3],
-					imagen: resumen[4].split(",//")[0],
-					color: () => {
-						let color = resumen[11];
-						if ( !color ) {
-							color = "no especifica";
-						}
-						return color;
-					},
-				};
-
-				data.push({
-					id: p.id, nombre: p.nombre, precio: p.precio(), fechaentrega: p.fechaentrega, imagen: p.imagen, color: p.color(),
-				});
-			}
-
-			setElproducto(data);
-			// setLoading(false);
-			// setTimeout(() => {
-			// 	setLoading(false);
-			// }, 200);
-		});
-	});
+			data.push( p );
+		}
+		setElproducto(data);
+	}, []);
 
 	if ( elproducto ) {
 		return (
@@ -63,9 +53,8 @@ function Tienda() {
 									key={prod.id}
 									id={prod.id}
 									nombre={prod.nombre}
-									precio={prod.precio}
-									fechaentrega={prod.fechaentrega}
-									color={prod.color}
+									precio={prod.precio()}
+									color={prod.color()}
 									imagen={prod.imagen}
 								/>
 							);
