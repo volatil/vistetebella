382970126f39a68f7cb1f4c3b5fb163bd8016800
Paulo <contacto@volatil.cl>
2023-02-08 21:53:18 -0300

    imprime algunos ID bajo el detalle producto

--- src/components/Relacionados/Relacionados.js
+++ src/components/Relacionados/Relacionados.js
@@ -1,15 +1,43 @@
-import { useEffect } from "react";
-import "./Relacionados.css";
+import { useEffect, useState } from "react";
+
+import Producto from "../Producto/Producto";
+
 import { traeData } from "../../Helpers/Helpers";
 
+import "./Relacionados.css";
+
 function Relacionados() {
+	const [elproducto, setElproducto] = useState();
 	useEffect(() => {
-		const ladata = traeData();
+		setElproducto( traeData() );
 	}, []);
 
 	return (
 		<section id="relacionados">
 			<h2>Relacionados</h2>
+			<section id="grilla" className="relacionados">
+				{
+					elproducto?.map( ( elprod ) => {
+						const algo = elprod.id;
+						const loselegidos = ["2", "5", "23", "32", "11"];
+						for ( let contador = 0; contador <= loselegidos.length - 1; contador++ ) {
+							if ( algo === loselegidos[contador] ) {
+								return (
+									<Producto
+										key={elprod.id}
+										id={elprod.id}
+										nombre={elprod.nombre}
+										imagen={elprod.imagen.principal}
+										precio={elprod.precio}
+										color={elprod.color()}
+									/>
+								);
+							}
+						}
+						return (null);
+					})
+				}
+			</section>
 		</section>
 	);
 }
